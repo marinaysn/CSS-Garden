416e8cc4ac52afaca9a1a0645e20bd533a8cdb07
Marina <marinaysn@gmail.com>
2019-07-10 19:56:46 -0400

    Liquid Image Transition

---
+++ ImagesHover Effects/Liquid Image Transition/app.js
@@ -0,0 +1,19 @@
+
+new hoverEffect({
+    parent: document.querySelector('.distortion'),
+    intensity: 0.4,
+    image1: './images/1.jpg',
+    image2: './images/2.jpg',
+    // angle: 0.785,
+    // speedIn: 1,
+    // speedOut: 2,
+
+    displacementImage: './images/fluid.jpg'
+})
+
+
+
+console.log("thank you for the Javascript library to draw and animate images on hover to: robin-dela/hover-effect");
+console.log("thank you Anastasia Vityukova for images: https://unsplash.com/@anastasiavitph")
+console.log("thank you Dev Ed for this great tutorial: https://www.youtube.com/watch?v=HlCzCq46YTk")
+console.log("Special Thank to all of you and many likes")
